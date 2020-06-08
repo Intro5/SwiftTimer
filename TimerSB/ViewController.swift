3791ee16140c4f4a5f7f265bebf52e71aee69f58
Torin Bakos <torinbakos@me.com>
2020-06-07 23:20:03 -0700

    Initial Commit

---
+++ TimerSB/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TimerSB
+//
+//  Created by Torin Bakos on 6/7/20.
+//  Copyright © 2020 Torin Bakos. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
